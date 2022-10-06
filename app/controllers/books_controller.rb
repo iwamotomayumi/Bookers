a2ac685fde5e41ec5e2badc546b60c5862753cab
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2022-10-06 04:52:27 +0000

    [update2 test追加]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
-    # 4. 詳細画面へリダイレクト
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -26,8 +28,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
 
 private
